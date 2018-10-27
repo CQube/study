4718fad3c7ece204abd6ef01670b9d53c24ded3a
Nikita Maltsev <n.maltsev.dis@gmail.com>
2018-10-27 22:07:03 +0300

    Added coefficients to config values

--- security/reliability/src/Config.py
+++ security/reliability/src/Config.py
@@ -1,5 +1,5 @@
 import json
-
+import numpy as np
 
 class ConfigReader:
     data = {}
@@ -9,9 +9,24 @@ class ConfigReader:
             with open(path, 'r') as f:
                 self.data = json.load(f)
 
+            for rate in self.data['denial_rates']:
+                rate *= np.power(10.0, -8)
+
+            self.data['beta'] *= 10
+
+            self.data['time'] *= np.power(10, 4) 
+
+            self.data['storing']['time'] *= np.power(10, 4)
+
+            self.data['storing']['fail_rate_drop_factor'] * np.power(10, 2)
+
+            self.data['cyclic_work']['time'] *= np.power(10, 4)
+
+            self.data['dt'] *= np.power(10, 3)
+
         except OSError:
-            print("Cannot open file")
+            print('Cannot open file')
 
 
-if __name__ != "__main__":
+if __name__ != '__main__':
     cfg = ConfigReader('../resource/config.json')
